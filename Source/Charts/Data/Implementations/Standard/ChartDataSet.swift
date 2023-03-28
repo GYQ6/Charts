1c80457cf15f3e5366059adccb5e55cc3ca3d9a1
gyq6 <18738193980@163.com>
2023-03-28 15:08:23 +0800

    格式调整

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -528,6 +528,7 @@ extension ChartDataSet: RandomAccessCollection {
 
 // MARK: RangeReplaceableCollection
 extension ChartDataSet: RangeReplaceableCollection {
+    
     /// 修复 XCode14 报错问题
     public func replaceSubrange<C>(_ subrange: Swift.Range<Int>, with newElements: C) where C : Collection, ChartDataEntry == C.Element {
         entries.replaceSubrange(subrange, with: newElements)
